f7f19042b5477d97cb9ff3135849ce267947cd26
gorewaiiieuo <super_yushao1996@qq.com>
2018-03-29 18:23:23 +0800

    3/29
    Ben(20153753) for the UI design

---
+++ app/src/main/java/guohuayu/com/account/addbillActivity.java
@@ -0,0 +1,18 @@
+package guohuayu.com.account;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+
+public class addbillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addbill);
+
+    }
+}
---
+++ app/src/main/java/guohuayu/com/account/addplanActivity.java
@@ -0,0 +1,13 @@
+package guohuayu.com.account;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addplanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addplan);
+    }
+}
